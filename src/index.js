d40ccd1f91361a22f11d19466c215b89cd070b0f
Gil Fewster <hello@gilfewster.com>
2019-07-29 16:21:09 +1000

    fullscreen video

--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import * as handTrack from 'handtrackjs';
 import { ELEMENTS, CONFIG } from './js/settings.js';
 
 const init = async () => {
-  setMsg('Hang on a minute. Setting up.');
+  var video = ELEMENTS.VIDEO;
 
+  setMsg('Hang on a minute. Setting up.');
   ELEMENTS.BTN_VIDEO_TOGGLE.onclick = e => toggleVideo();
   ELEMENTS.BTN_VIDEO_TOGGLE.disabled = true;
   const model = await loadTrackingModel();
-
   ELEMENTS.BTN_VIDEO_TOGGLE.disabled = false;
   startVideo();
   update(model, ELEMENTS.CANVAS.getContext('2d'));
-  setMsg('');
-};
-
-const onResize = () => {
-  const v = ELEMENTS.VIDEO;
-  const c = ELEMENTS.CANVAS;
+  setMsg('HELLO!');
 };
 
 const loadTrackingModel = () => {
@@ -35,18 +30,16 @@ const startVideo = () => {
 
 const update = (model, context) => {
   model.detect(ELEMENTS.VIDEO).then(predictions => {
-    setMsg(JSON.stringify(predictions));
-    // model.renderPredictions(predictions, ELEMENTS.CANVAS, context, ELEMENTS.VIDEO);
-
-    // var canvas = document.getElementById("myCanvas");
-    // var context = canvas.getContext("2d");
-
     if (predictions[0]) {
       const { bbox } = predictions[0];
+
       const x = bbox[0];
       const y = bbox[1];
       const w = bbox[2];
       const h = bbox[3];
+
+      setMsg(`${x}\n${y}\n${w}\n${h}`);
+
       context.clearRect(0, 0, ELEMENTS.CANVAS.width, ELEMENTS.CANVAS.height);
       context.strokeStyle = '#00FF00';
       context.rect(x, y, w, h);
