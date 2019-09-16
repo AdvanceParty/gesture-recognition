389ee2f420be55b98443742f25016d83a5d88ec4
Gil Fewster <hello@gilfewster.com>
2019-09-16 17:20:34 +1000

    using keras for training in the browser via webcan.\nToDo: componentise UI for easier code navigation.\nToDo: Implement a way of saving and loading trained models.

---
+++ src/js/classes/WebcamConnector.js
@@ -0,0 +1,97 @@
+const isSupported = () => navigator.mediaDevices && navigator.mediaDevices.getUserMedia;
+const unsupportedMsg = 'Browser API navigator.mediaDevices.getUserMedia not available';
+
+export class WebcamConnector {
+  static get FacingMode() {
+    return { ...facingMode };
+  }
+
+  constructor({
+    width = 300,
+    height = 250,
+    videoElement = null,
+    audio = false,
+    facingMode = WebcamConnector.FacingMode.User,
+  } = {}) {
+    this.width = width;
+    this.height = height;
+    this.facingMode = facingMode;
+    this.audio = audio;
+
+    if (videoElement) {
+      this.videoElement = videoElement;
+    }
+  }
+
+  async start() {
+    if (!isSupported) {
+      throw new Error(unsupportedMsg);
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia(this.options);
+    this.videoElement.srcObject = stream;
+    return new Promise(resolve => {
+      this.videoElement.onloadedmetadata = () => {
+        resolve(this.videoElement);
+      };
+    });
+  }
+
+  get options() {
+    return {
+      video: {
+        width: this.width,
+        height: this.height,
+        facingMode: this.facingMode,
+      },
+      audio: this.audio,
+    };
+  }
+
+  set facingMode(str) {
+    this._facingMode = str;
+  }
+  get facingMode() {
+    return this._facingMode;
+  }
+
+  set width(width) {
+    this._width = width;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  set height(height) {
+    this._height = height;
+  }
+
+  get height() {
+    return this._height;
+  }
+  set audio(val) {
+    this._audio = val;
+  }
+
+  get audio() {
+    return this._audio;
+  }
+
+  set videoElement(el) {
+    this._videoElement = el;
+  }
+  get videoElement() {
+    return this._videoElement;
+  }
+}
+
+const facingMode = {
+  User: 'user',
+  Selfie: 'user',
+  Reverse: 'user',
+  Rear: 'user',
+  Environment: 'environment',
+  Front: 'environment',
+  Main: 'environment',
+};
